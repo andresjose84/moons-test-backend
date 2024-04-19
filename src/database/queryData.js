ea1a6d685e2f47bce7a79868dc1039bae59597e0
Andres Jose Sanchez <andresjose84@gmail.com>
2024-04-18 23:19:10 -0500

    Se completaron los campos necesarios para crear las cards en el frontend

--- src/database/queryData.js
+++ src/database/queryData.js
@@ -28,33 +28,58 @@ const queryData = async params => {
     try {
 
         const results = await query.find();
+        const horario = {
+            'weekdays': 'L-V',
+            'saturday': 'S',
+            'sunday': 'D'
+        };
 
         for ( const object of results ) {
-            const res = {
-                Center_Name: object.get( 'Center_Name' ),
-                Center_Type: object.get( 'Center_Type' ),
-                Zone: object.get( 'Zone' ),
-                Direccion: "",
-                Services: object.get( 'Services' ),
-                Calendar_Id: object.get( 'Calendar_Id' ),
-                Appointment_Type_Id: object.get( 'Appointment_Type_Id' )
-            };
+            const zone = object.get( 'Zone' );
+            if ( object.get( 'Zone' ) ) {
+                const res = {
+                    Center_Name: object.get( 'Center_Name' ),
+                    Center_Type: object.get( 'Center_Type' ),
+                    Zone: zone,
+                    Timetable: object.get( 'Timetable' ),
+                    Promo: object.get( 'promo' ),
+                    Direccion: `${ object.get( 'Street' ) } ${ object.get( 'Number' ) }, ${ object.get( 'Neighborhood' ) }`,
+                    Services: object.get( 'Services' ),
+                    Calendar_Id: object.get( 'Calendar_Id' ),
+                    Appointment_Type_Id: object.get( 'Appointment_Type_Id' )
+                };
 
-            //Cambiamos los datos de Appointment_Type_Id si en la busqueda llega el servicio
-            if ( services )
-                res.Appointment_Type_Id = !res.Services[ services ].AppointmentTypeId ? res.Appointment_Type_Id : res.Services[ services ].AppointmentTypeId;
+                if ( res.Timetable ) {
+                    res.Horario = Object.keys( res.Timetable ).reduce( ( acc, current ) => {
+                        if ( acc === '' ) {
+                            return `${ acc }  ${ horario[ current ] } ${ res.Timetable[ current ][ 0 ] }`
+                        } else {
+                            return `${ acc } / ${ horario[ current ] } ${ res.Timetable[ current ][ 0 ] }`
+                        }
+                    }, '' );
+                } else {
+                    res.Horario = '';
+                }
 
-            if ( !data[ res.Zone ] )
-                data[ res.Zone ] = [];
 
-            data[ res.Zone ].push( res )
+                //Cambiamos los datos de Appointment_Type_Id si en la busqueda llega el servicio
+                if ( services )
+                    res.Appointment_Type_Id = !res.Services[ services ].AppointmentTypeId ? res.Appointment_Type_Id : res.Services[ services ].AppointmentTypeId;
+
+                if ( !data[ res.Zone ] )
+                    data[ res.Zone ] = [];
+
+                data[ res.Zone ].push( res )
+            }
+
+
 
         }
 
         return data;
     } catch ( error ) {
-        console.log( 'error query', JSON.stringify( error, null, 2 ) );
-        return [];
+        console.log( 'error query', error );
+        return {};
     }
 
 }
